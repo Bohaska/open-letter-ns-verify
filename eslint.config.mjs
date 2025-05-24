a387886b2ff8103ac5b5eea840a456839e52487f
Bohaska <73286691+Bohaska@users.noreply.github.com>
2025-05-25 07:59:06 +0800

    ignore eslint when building

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   }),
 ]
 
